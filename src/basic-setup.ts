ae5d774e7dfe36c8c47665aa60fc6a0b4b2ce288
Michiel Dral <m.c.dral@gmail.com>
2021-12-08 17:28:15 +0000

    ???

--- src/basic-setup.ts
+++ src/basic-setup.ts
@@ -1,49 +1,22 @@
-import {
-  EditorState,
-  EditorSelection,
-  Compartment,
-  SelectionRange,
-  Facet,
-  StateField,
-  StateEffect,
-  Transaction,
-  Text,
-  combineConfig,
-  Prec,
-} from "@codemirror/state";
+import * as state from "@codemirror/state";
+import * as view from "@codemirror/view";
+import * as autocomplete from "@codemirror/autocomplete";
+import * as highlight from "@codemirror/highlight";
+import * as lezer_common from "@lezer/common";
+import * as language from "@codemirror/language";
+import * as commands from "@codemirror/commands";
+
 import { StreamLanguage } from "@codemirror/stream-parser";
 import { julia as julia_legacy } from "@codemirror/legacy-modes/mode/julia";
 import { julia as julia_andrey } from "lang-julia";
 import { lineNumbers } from "@codemirror/gutter";
-import {
-  keymap,
-  EditorView,
-  highlightSpecialChars,
-  drawSelection,
-  highlightActiveLine,
-  placeholder,
-  Decoration,
-  ViewUpdate,
-  ViewPlugin,
-  WidgetType,
-  PluginField,
-} from "@codemirror/view";
 import { historyKeymap, history } from "@codemirror/history";
-import { defaultKeymap, indentMore, indentLess } from "@codemirror/commands";
-import {
-  defaultHighlightStyle,
-  tags,
-  HighlightStyle,
-} from "@codemirror/highlight";
-import { indentOnInput, indentUnit, syntaxTree } from "@codemirror/language";
 import { rectangularSelection } from "@codemirror/rectangular-selection";
 import { foldGutter, foldKeymap } from "@codemirror/fold";
 import { bracketMatching } from "@codemirror/matchbrackets";
 import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets";
 import { highlightSelectionMatches, searchKeymap } from "@codemirror/search";
-import * as autocomplete from "@codemirror/autocomplete";
 import { commentKeymap } from "@codemirror/comment";
-import { TreeCursor, NodeProp, parseMixed } from "@lezer/common";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { html, htmlLanguage } from "@codemirror/lang-html";
 import { javascript, javascriptLanguage } from "@codemirror/lang-javascript";
@@ -55,35 +28,20 @@ export let autocompletion = autocomplete.autocompletion;
 export let completionKeymap = autocomplete.completionKeymap;
 
 export {
-  Facet,
-  StateField,
-  StateEffect,
-  Transaction,
-  indentUnit,
-  EditorState,
-  EditorSelection,
-  Compartment,
-  EditorView,
-  SelectionRange,
-  placeholder,
+  state,
+  view,
+  autocomplete,
+  highlight,
+  lezer_common,
+  language,
+  commands,
   julia_legacy,
   julia_andrey,
-  keymap,
   history,
   historyKeymap,
-  defaultKeymap,
   StreamLanguage,
-  indentMore,
-  indentLess,
-  tags,
-  HighlightStyle,
-  syntaxTree,
   lineNumbers,
-  highlightSpecialChars,
   foldGutter,
-  drawSelection,
-  indentOnInput,
-  defaultHighlightStyle,
   bracketMatching,
   closeBrackets,
   rectangularSelection,
@@ -92,18 +50,8 @@ export {
   searchKeymap,
   foldKeymap,
   commentKeymap,
-  autocomplete,
-  Decoration,
-  ViewUpdate,
-  ViewPlugin,
-  WidgetType,
-  TreeCursor,
-  Text,
-  combineConfig,
-  NodeProp,
   markdown,
   markdownLanguage,
-  parseMixed,
   html,
   htmlLanguage,
   javascript,
@@ -112,6 +60,4 @@ export {
   PostgreSQL,
   python,
   pythonLanguage,
-  PluginField,
-  Prec,
 };
