9b78be05f8e33ab620448fbf6b888b2ab7bec619
Michiel Dral <m.c.dral@gmail.com>
2022-01-19 22:27:52 +0000

    uhhh

--- src/basic-setup.ts
+++ src/basic-setup.ts
@@ -10,11 +10,11 @@ import {
   Text,
   combineConfig,
   Annotation,
-} from "@codemirror/state";
-import { StreamLanguage } from "@codemirror/stream-parser";
-import { julia as julia_legacy } from "@codemirror/legacy-modes/mode/julia";
-import { julia as julia_andrey } from "@plutojl/lang-julia";
-import { lineNumbers } from "@codemirror/gutter";
+} from "@codemirror/state"
+import { StreamLanguage } from "@codemirror/stream-parser"
+import { julia as julia_legacy } from "@codemirror/legacy-modes/mode/julia"
+import { julia as julia_andrey } from "@plutojl/lang-julia"
+import { lineNumbers } from "@codemirror/gutter"
 import {
   keymap,
   EditorView,
@@ -26,31 +26,31 @@ import {
   ViewUpdate,
   ViewPlugin,
   WidgetType,
-} from "@codemirror/view";
-import { historyKeymap, history } from "@codemirror/history";
-import { defaultKeymap, indentMore, indentLess } from "@codemirror/commands";
+} from "@codemirror/view"
+import { historyKeymap, history } from "@codemirror/history"
+import { defaultKeymap, indentMore, indentLess } from "@codemirror/commands"
 import {
   defaultHighlightStyle,
   tags,
   HighlightStyle,
-} from "@codemirror/highlight";
-import { indentOnInput, indentUnit, syntaxTree } from "@codemirror/language";
-import { rectangularSelection } from "@codemirror/rectangular-selection";
-import { foldGutter, foldKeymap } from "@codemirror/fold";
-import { bracketMatching } from "@codemirror/matchbrackets";
-import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets";
-import * as autocomplete from "@codemirror/autocomplete";
-import { highlightSelectionMatches, searchKeymap } from "@codemirror/search";
-import { completionKeymap } from "@codemirror/autocomplete";
-import { commentKeymap } from "@codemirror/comment";
-import { TreeCursor, NodeProp, parseMixed } from "@lezer/common";
-import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
-import { parseCode } from "@lezer/markdown";
-import { html, htmlLanguage } from "@codemirror/lang-html";
-import { javascript, javascriptLanguage } from "@codemirror/lang-javascript";
-import { sql, PostgreSQL } from "@codemirror/lang-sql";
-import { python, pythonLanguage } from "@codemirror/lang-python";
-import { collab } from "@codemirror/collab";
+} from "@codemirror/highlight"
+import { indentOnInput, indentUnit, syntaxTree } from "@codemirror/language"
+import { rectangularSelection } from "@codemirror/rectangular-selection"
+import { foldGutter, foldKeymap } from "@codemirror/fold"
+import { bracketMatching } from "@codemirror/matchbrackets"
+import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets"
+import * as autocomplete from "@codemirror/autocomplete"
+import { highlightSelectionMatches, searchKeymap } from "@codemirror/search"
+import { completionKeymap } from "@codemirror/autocomplete"
+import { commentKeymap } from "@codemirror/comment"
+import { TreeCursor, NodeProp, parseMixed } from "@lezer/common"
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown"
+import { parseCode } from "@lezer/markdown"
+import { html, htmlLanguage } from "@codemirror/lang-html"
+import { javascript, javascriptLanguage } from "@codemirror/lang-javascript"
+import { sql, PostgreSQL } from "@codemirror/lang-sql"
+import { python, pythonLanguage } from "@codemirror/lang-python"
+import { collab } from "@codemirror/collab"
 
 export {
   Facet,
@@ -116,4 +116,4 @@ export {
   // Collaboration
   collab,
   Annotation,
-};
+}
